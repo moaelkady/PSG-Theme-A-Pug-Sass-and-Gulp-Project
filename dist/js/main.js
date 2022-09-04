ad7a28758a1241ec8491ead196da85fbf068e763
Mohamed elkady <mohameda.elqady@gmail.com>
2022-09-04 18:59:18 +0200

    edits to nav and sidebar

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,7 @@
-const mySet = new Set;
+//function to add active class to current page
+$('.page .header ul li a').filter(function(){
+    return this.href === location.href;
+    
+  }).addClass('active');
+
 const myMap = new Map;
