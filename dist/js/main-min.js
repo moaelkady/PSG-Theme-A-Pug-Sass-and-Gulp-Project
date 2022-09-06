2e7079bb3b99c02a6ecce39f59b2c5a1dd8af35a
Mohamed elkady <mohameda.elqady@gmail.com>
2022-09-06 14:06:11 +0200

    add order banner with styles

--- dist/js/main-min.js
+++ dist/js/main-min.js
@@ -1,1 +1,1 @@
-$(".page .header ul li a").filter(function(){return this.href===location.href}).addClass("active");const myMap=new Map;
+$(".page .header ul li a").filter(function(){return this.href===location.href}).addClass("active"),$(window).scroll(function(){$(this).scrollTop()?$("#toTop").fadeIn():$("#toTop").fadeOut()}),$("#toTop").click(function(){$("html, body").animate({scrollTop:0},1e3)});const myMap=new Map;
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,22 @@
 //function to add active class to current page
-$('.page .header ul li a').filter(function(){
+$(".page .header ul li a")
+  .filter(function () {
     return this.href === location.href;
-    
-  }).addClass('active');
+  })
+  .addClass("active");
+
+//function for button to go top
+
+$(window).scroll(function () {
+  if ($(this).scrollTop()) {
+    $("#toTop").fadeIn();
+  } else {
+    $("#toTop").fadeOut();
+  }
+});
+
+$("#toTop").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, 1000);
+});
 
 const myMap = new Map;
