66e7e70b73b14df224cca9d64bb21e0e8003918d
Mohamed elkady <mohameda.elqady@gmail.com>
2022-09-07 18:18:22 +0200

    edits to main page

--- dist/js/main.js
+++ dist/js/main.js
@@ -21,8 +21,29 @@ $("#toTop").click(function () {
 
 //function for mobile menu
 
-$(".mobile-menu").click(function(){
-  $(".header ul").toggleClass('show');
-  $(".mobile-menu").toggleClass('bars-x');
+$(".mobile-menu").click(function () {
+  $(".header ul").toggleClass("show");
+  $(".mobile-menu").toggleClass("bars-x");
 });
+
+//function to scroll links inside page smoothly
+$('a[href*="#"]:not([href="#"])').click(function () {
+  if (
+    location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
+    location.hostname == this.hostname
+  ) {
+    var target = $(this.hash);
+    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+    if (target.length) {
+      $("html, body").animate(
+        {
+          scrollTop: target.offset().top,
+        },
+        500
+      );
+      return false;
+    }
+  }
+});
+
 const myMap = new Map;
